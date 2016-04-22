3841f58a13de249930a274d96df2fb0ed545bdd6
presisco <internight@sina.com>
2016-04-22 09:49:59 +0800

    add network samples

---
+++ app/src/main/java/product/presisco/yourdrivers/Network/Task/Constants.java
@@ -0,0 +1,8 @@
+package product.presisco.yourdrivers.Network.Task;
+
+/**
+ * Created by presisco on 2016/4/20.
+ */
+public class Constants {
+}
+
--- app/src/main/java/product/presisco/yourdrivers/Network/Task/GetTopics.java
+++ app/src/main/java/product/presisco/yourdrivers/Network/Task/GetTopics.java
@@ -3,10 +3,16 @@ package product.presisco.yourdrivers.Network.Task;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.util.Pair;
+import android.util.Xml;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -79,5 +85,94 @@ public class GetTopics extends AsyncTask<Void, Void, List<Topic>> {
 
     public interface OnLoadCompleteListener {
         void onLoadComplete(List<Topic> src);
+    }
+
+    private class TopicsListParser {
+        private final String ns = null;
+        private String path[];
+        private String elementEntry;
+
+        public void parse(InputStream in, String _path[], String entry) throws XmlPullParserException, IOException {
+            try {
+                path = _path;
+                elementEntry = entry;
+                XmlPullParser parser = Xml.newPullParser();
+                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                parser.setInput(in, null);
+                parser.nextTag();
+                readFeed(parser, 0);
+            } finally {
+                in.close();
+            }
+        }
+
+        private void readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+            parser.require(XmlPullParser.START_TAG, ns, "html");
+            while (parser.next() != XmlPullParser.END_TAG) {
+                if (parser.getEventType() != XmlPullParser.START_TAG) {
+                    continue;
+                }
+                String name = parser.getName();
+                // Starts by looking for the entry tag
+                if (name.equals("body")) {
+                    break;
+                } else {
+                    skip(parser);
+                }
+            }
+        }
+
+        private void readFeed(XmlPullParser parser, int depth) throws XmlPullParserException, IOException {
+            parser.require(XmlPullParser.START_TAG, ns, path[depth++]);
+            while (parser.next() != XmlPullParser.END_TAG) {
+                if (parser.getEventType() != XmlPullParser.START_TAG) {
+                    continue;
+                }
+                String name = parser.getName();
+                // Starts by looking for the entry tag
+                if (depth == path.length) {
+                    if (name.equals(elementEntry))
+                        readData(parser);
+                    else
+                        skip(parser);
+                } else if (name.equals(path[depth])) {
+                    readFeed(parser, depth);
+                } else {
+                    skip(parser);
+                }
+            }
+        }
+
+        private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                throw new IllegalStateException();
+            }
+            int depth = 1;
+            while (depth != 0) {
+                switch (parser.next()) {
+                    case XmlPullParser.END_TAG:
+                        depth--;
+                        break;
+                    case XmlPullParser.START_TAG:
+                        depth++;
+                        break;
+                }
+            }
+        }
+
+        // Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them off
+        // to their respective "read" methods for processing. Otherwise, skips the tag.
+        private void readData(XmlPullParser parser) throws XmlPullParserException, IOException {
+            parser.require(XmlPullParser.START_TAG, ns, "input");
+            String name = parser.getAttributeValue(null, "name");
+            String id = parser.getAttributeValue(null, "id");
+            if (name.equals("__VIEWSTATE") && id.equals("__VIEWSTATE")) {
+                //    view_state=parser.getAttributeValue(null,"value");
+            } else if (name.equals("__EVENTVALIDATION") && id.equals("__EVENTVALIDATION")) {
+                //    event_validation=parser.getAttributeValue(null,"value");
+            }
+            parser.nextTag();
+            parser.require(XmlPullParser.END_TAG, ns, "input");
+        }
     }
 }
